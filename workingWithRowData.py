659d37e38c92cdf579a70ad88f450f8b2b6d2519
MatiasMoore <nikita.birukov.03@mail.ru>
2023-05-28 23:32:31 +0300

    Merge branch hi2Observed
    commit d0037ad922d5cf585638cea0ae5b062aa69b7c39
    Merge: 3d4f524 37678ec
    Author: Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
    Date:   Sun May 28 23:31:38 2023 +0300
    
        Merge branch 'hi2Observed' of https://github.com/T-LORE/tv_lr6 into hi2Observed
    
    commit 3d4f524dbf81e18a34cc80e2cabfca3a71303da9
    Author: Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
    Date:   Sun May 28 23:29:07 2023 +0300
    
        Сделал вычисление критическое значение χ2
    
    commit 37678ecec8fecd1aa2b22519d55ab015eb263af9
    Author: MatiasMoore <nikita.birukov.03@mail.ru>
    Date:   Sun May 28 23:19:30 2023 +0300
    
        Базовая поддержка второго задания
    
        ВНИМАНИЕ подключено к первой кнопке, т.к. вторая занята первым заданием
    
    commit ec39c4eaa33445b3868c5998f0e1cf0c31cffd19
    Author: Maxim Zverkov <75385902+T-LORE@users.noreply.github.com>
    Date:   Sun May 28 22:56:16 2023 +0300
    
        Update workingWithRowData.py

--- workingWithRowData.py
+++ workingWithRowData.py
@@ -1,6 +1,8 @@
 import numpy as np
 from graphingFunctions import roundValue
 from scipy import integrate
+import scipy.stats as stats  
+from math import factorial, exp 
 #перечисление рядов (частот, относительныйх частот)
 
 class RowType:
@@ -45,6 +47,14 @@ def getSigma(numbersArray):
 def getS(numbersArray):
     return (1/(len(numbersArray) - 1)) * np.sum(numbersArray )
 
+def getLamda(puasonRow):
+    return sum([pair[0] * pair[1] for pair in zip(puasonRow['x'], puasonRow['m'])]) / sum(puasonRow['m'])
+
+def getTheoreticalProbability(lambd, xi):
+    print(lambd ** xi)
+    print(exp(-lambd))
+    print(factorial(int(xi)))
+    return (lambd ** xi * exp(-lambd)) / factorial(int(xi))
 
 #Разбить на минимальное количество интервалов, чтобы частота каждого интервала была не меньше заданной
 def split_array_into_intervals(arr, min_frequency):
@@ -144,7 +154,7 @@ def split(arr, maxIntervals):
     # return intervals
 
     
-def simpson_rule(func, start, end, n):
+def simpsonRule(func, start, end, n):
     h = (end - start) / (2 * n)
     x = [start + i * h for i in range(2 * n + 1)]
     y = [func(x[i]) for i in range(2 * n + 1)]
@@ -159,17 +169,38 @@ def getPi(aStar, sigmaStar, intervals):
     for i in range(len(intervals['start'])):
         startIntegral = (intervals['start'][i]- aStar) / sigmaStar
         endIntegral = (intervals['end'][i] - aStar) / sigmaStar   
-        pi.append(roundValue(coef * simpson_rule(underIntegral, startIntegral, endIntegral, 1000)))
+        pi.append(roundValue(coef * simpsonRule(underIntegral, startIntegral, endIntegral, 5)))
     return pi
 
 def libGetPi(aStar, sigmaStar, intervals):
-    underIntegral = lambda x: np.exp(-(x ** 2) /2)
+    underIntegral = lambda t: np.exp(-(t ** 2) /2)
     pi = []
     coef = 1 / (np.sqrt(2 * np.pi))
     for i in range(len(intervals['start'])):
-        startIntegral = (intervals['start'][i]- aStar) / sigmaStar
-        endIntegral = (intervals['end'][i] - aStar) / sigmaStar   
-        pi.append(roundValue(coef * integrate.fixed_quad(underIntegral, startIntegral, endIntegral, n = 1000)[0]))
+        u1 = (intervals['start'][i]- aStar) / sigmaStar
+        u2  = (intervals['end'][i] - aStar) / sigmaStar
+        Fu1 = coef * integrate.fixed_quad(underIntegral, 0, u1, n = 2)[0]
+        Fu2 = coef * integrate.fixed_quad(underIntegral, 0, u2, n = 2)[0]
+        pi.append(round(Fu2 - Fu1, 5))
     return pi
-    
-    return 
+       
+def gethi2ObservedArray(frequency, p):
+    hi2Observed = []
+    for i in range(len(p)):
+        hi2Observed.append(((frequency[i] - p[i] * sum(frequency)) ** 2) / (p[i] * sum(frequency)))
+    return hi2Observed
+
+def gethi2CriticalArray(k, a):
+    hi2CriticalMap = {
+        '0.01' : [6.6,9.2,11.3,13.3,15.1,16.8,18.5,20.1,21.7,23.2,24.7,26.2,27.7,29.1,30.6,32,33.4,34.8,36.2,37.6,38.9,40.3,41.6,43,44.3,45.6,47,48.3,49.6,50.9],
+        '0.025': [5,7.4,9.4,11.1,12.8,14.4,16,17.5,19,20.5,21.9,23.3,24.7,26.1,27.5,28.8,30.2,31.5,32.9,34.2,35.5,36.8,38.1,39.4,40.6,41.9,43.2,44.5,45.7,47],
+        '0.05' : [3.8,6,7.8,9.5,11.1,12.6,14.1,15.5,16.9,18.3,19.7,21.0,22.4,23.7,25,26.3,27.6,28.9,30.1,31.4,32.7,33.9,35.2,36.4,37.7,38.9,40.1,41.3,42.6,43.8],
+        '0.95' : [0.0039,0.103,0.352,0.711,1.15,1.64,2.17,2.73,3.33,3.94,4.57,5.23,5.89,6.57,7.26,7.96,8.67,9.39,10.1,10.9,11.6,12.3,13.1,13.8,14.6,15.4,16.2,16.9,17.7,18.5],
+        '0.975': [0.00098,0.051,0.216,0.484,0.831,1.24,1.69,2.18,2.7,3.25,3.82,4.4,5.01,5.63,6.26,6.91,7.56,8.23,8.91,9.59,10.3,11,11.7,12.4,13.1,13.8,14.6,15.3,16,16.8],
+        '0.99' : [0.00016,0.02,0.115,0.297,0.554,0.872,1.24,1.65,2.09,2.56,3.05,3.57,4.11,4.66,5.23,5.81,6.41,7.01,7.63,8.26,8.9,9.54,10.2,10.9,11.5,12.2,12.9,13.6,14.3,15]  
+    }
+    return hi2CriticalMap[str(a)][k-1]
+
+
+def libGethi2CriticalArray(k, a):
+    return stats.chi2.ppf(1-a, k)  
